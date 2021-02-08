46e4c5710588123b9d682dcdcbd3c63149863a26
Filip <36089495+magalek@users.noreply.github.com>
2021-02-08 18:04:47 +0100

    Update main.py

--- src/main.py
+++ src/main.py
@@ -6,7 +6,7 @@ url = 'https://store.steampowered.com/search/?sort_by=Released_DESC&category3=1'
 
 second_url = 'https://store.steampowered.com/search/?category3=1'
 
-discord_url = 'https://discord.com/api/webhooks/783337412801331210/4G09CQYlUZtt9WGXl-lGh6ECHyrm_bJAwo-yHLghTh5ds1gfa1iVQxXGig9LcJEfuYqn'
+discord_url = '' 
 
 
 page = requests.get(second_url)
@@ -25,19 +25,7 @@ for game_html in games_iterator:
             game_info = f'[{game.name}](<{game.link}\>)\nNot available\n'
     else:
         game_info = f'###[{game.name}](<{game.link}\>)\n~~Normal price: {game.price}zł~~\n**Price on discount: {game.discounted_price}zł**\n'
-    #print(game_info)
-    data = {'content' : game_info}
-    requests.post(discord_url, data=data)
-    # print(game.name)
-    # print(game.appid)
-    # # print(game.link)
-    # print(game.price)
-    # if (game.discounted_price != '-1'):
-    #     print(game.discounted_price)
-    # print('\n')
 
-
-#data = {'content' : 69}
-
-
-#requests.post(discord_url, data=data)
+    # UNCOMMENT IF DISCORD HOOK ADDED
+    # data = {'content' : game_info}
+    # requests.post(discord_url, data=data)
